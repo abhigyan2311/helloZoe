f2bf4a892a7d09cd8b1451558a359874554848c7
pratik Deo <pratikdeo78@gmail.com>
2020-10-20 20:05:08 -0400

    added addTasks.handlebars

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const data = require('../data');
+const data = require("../data");
 
-router.get('/managetasks', async (req, res) => {
+router.get("/managetasks", async (req, res) => {
   if (!req.session.user) {
-    res.render('home/accessDenied', { title: 'Access Denied' });
+    res.render("home/accessDenied", { title: "Access Denied" });
     return;
   }
 
-  res.render('tasks/manageTasks', { title: 'Manage Tasks' });
+  res.render("tasks/manageTasks", { title: "Manage Tasks" });
 });
 
 module.exports = router;
